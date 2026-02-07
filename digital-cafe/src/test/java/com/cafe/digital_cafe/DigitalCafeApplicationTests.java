7894e3ce5b609c6c8b9c7f5399f58fc5bd8c451e
Hema Vavilapalli <hemalathavavilapalli08@gmail.com>
2026-02-07 23:42:39 +0530

    firs tcommit

---
+++ digital-cafe/src/test/java/com/cafe/digital_cafe/DigitalCafeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cafe.digital_cafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalCafeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
